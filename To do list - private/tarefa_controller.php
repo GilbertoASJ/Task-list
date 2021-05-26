e985be0956a22de5aade62463d307cb0ea6d6014
Gilberto Alves <alvesgilberto84@gmail.com>
2021-05-26 15:21:15 -0300

    Updating files

--- To do list - private/tarefa_controller.php
+++ To do list - private/tarefa_controller.php
@@ -7,21 +7,25 @@
 	require '../../To do list - private/tarefa.service.php';
 	require '../../To do list - private/conexao.php';
 
-	echo "<pre>";
-		print_r($_POST);
-	echo "</pre>";
+	// Atribuindo a variável ação o seu respectivo valor, caso exista na superglobal Get, caso não exista irá receber de algum outro lugar.
 
-	// Instâncinado as classes contidas dentro de cada arquivo:
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
+	if ($acao == 'inserir') {
+		// Instâncinado as classes contidas dentro de cada arquivo:
 
-	$conexao = new Conexao();
-	
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->inserir();
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
 
-	echo "<pre>";
-		print_r($tarefaService);
-	echo "</pre>";
+		$conexao = new Conexao();
+		
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
+
+		header('Location: nova_tarefa.php?inclusao=1');
+
+	} else if ($acao == 'recuperar') {
+		echo "Chegamos até aqui";
+	}
+
 ?>
