c1c1a47884cbd8a75096c40d7f9a065d2d8ab0b8
Gilberto Alves <alvesgilberto84@gmail.com>
2021-05-28 17:21:40 -0300

    Updating files

--- To do list/todas_tarefas.php
+++ To do list/todas_tarefas.php
@@ -20,6 +20,12 @@
 
 		<!-- Fontawesome -->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<style type="text/css">
+			i {
+				cursor: pointer;
+			}
+		</style>
 	</head>
 
 	<body>
@@ -52,12 +58,18 @@
 								<?php foreach ($tarefas as $indice => $tarefa) { ?>
 
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9">
+
+										<div id="tarefa_<?=$tarefa["id"]?>" class="col-sm-9">
 											<?= $tarefa['tarefa'] ?> (<?= $tarefa['status'] ?>)
 										</div>
+
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+
+											<i 
+												class="fas fa-edit fa-lg text-info" 
+												onclick="editarTarefa(<?=$tarefa["id"]?>, '<?= $tarefa['tarefa'] ?>')"
+											></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 									</div>
@@ -70,5 +82,47 @@
 				</div>
 			</div>
 		</div>
+
+		<script type="text/javascript">
+			
+			function editarTarefa(id, tarefaAntiga) {
+
+				// Criar um form de edição
+				let form = document.createElement('form');
+				form.action = 'tarefa_controller.php?acao=atualizar';
+				form.method = 'post';
+				form.className = 'row';
+
+				// Criar um input para a entrada do texto
+				let inputTarefa = document.createElement('input');
+				inputTarefa.type = 'text';
+				inputTarefa.name = 'tarefaEditada';
+				inputTarefa.className = 'col-9 form-control';
+				inputTarefa.value = tarefaAntiga;
+
+				// Criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input');
+				inputId.type = 'hidden';
+				inputId.name = 'id';
+				inputId.value = id;
+
+				// Criar um button para o envio do form
+				let button = document.createElement('button');
+				button.type = 'submit';
+				button.className = 'col-3 btn btn-info';
+				button.innerHTML = 'Atualizar'
+
+				// Incluindo os elementos dentro de um contexto html
+				form.appendChild(inputTarefa);
+				form.appendChild(button);
+				form.appendChild(inputId);
+
+				// Selecionando e incluindo o form dentro da div.
+				let tarefa = document.querySelector(`#tarefa_${id}`);
+				tarefa.innerHTML = "";
+				tarefa.insertBefore(form, tarefa[0]);
+			}
+
+		</script>
 	</body>
 </html>
