a0f0175bd73e655190c8f5d68e8e414ec8fff78e
Gilberto Alves <alvesgilberto84@gmail.com>
2021-05-29 12:14:31 -0300

    Updating files

--- To do list/todas_tarefas.php
+++ To do list/todas_tarefas.php
@@ -64,7 +64,11 @@
 										</div>
 
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
-											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+
+											<i 
+												class="fas fa-trash-alt fa-lg text-danger"
+												onclick="removerTarefa(<?=$tarefa["id"]?>)" 
+											></i>
 
 											<i 
 												class="fas fa-edit fa-lg text-info" 
@@ -121,6 +125,10 @@
 				let tarefa = document.querySelector(`#tarefa_${id}`);
 				tarefa.innerHTML = "";
 				tarefa.insertBefore(form, tarefa[0]);
+			}
+
+			function removerTarefa(id) {
+				window.location = `todas_tarefas.php?acao=remover&id=${id}`
 			}
 
 		</script>
